3818685d9989098d6c04672632c3cf26f7897fd1
Julia Martsenko <jmartsenko@gmil.com>
2021-05-23 17:05:45 +0300

    completed 1.1

--- Objc_Task3/ios.stage-task/Exercises/Exercise-1/DateHelper.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-1/DateHelper.m
@@ -5,6 +5,20 @@
 #pragma mark - First
 
 -(NSString *)monthNameBy:(NSUInteger)monthNumber {
+    if (monthNumber > 0 && monthNumber <= 12) {
+        NSString *dateNumString = [NSString stringWithFormat: @"%lu", monthNumber];
+        NSDateFormatter* dateNumFormat = [NSDateFormatter new];
+        if (monthNumber < 10) {
+            [dateNumFormat setDateFormat:@"M"];
+        }
+        else {
+            [dateNumFormat setDateFormat:@"MM"];
+        }
+        NSDate* date = [dateNumFormat dateFromString:dateNumString];
+        NSDateFormatter *dateStringFormat = [NSDateFormatter new];
+        [dateStringFormat setDateFormat:@"MMMM"];
+        return [dateStringFormat stringFromDate:date];
+    }
     return nil;
 }
 

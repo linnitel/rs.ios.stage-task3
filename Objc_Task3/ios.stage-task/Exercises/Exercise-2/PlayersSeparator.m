a3dc6f9a6a10829f0672ea01dedc5718f693188d
Julia Martsenko <jmartsenko@gmil.com>
2021-05-24 04:23:42 +0300

    task two completed

--- Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
@@ -3,7 +3,17 @@
 @implementation PlayersSeparator
 
 - (NSInteger)dividePlayersIntoTeams:(NSArray<NSNumber *>*)ratingArray {
-    return 0;
+    NSInteger number = 0;
+    for (NSInteger index = 0; index + 2 < ratingArray.count; index++) {
+        for (NSInteger indexTwo = 1;  index + indexTwo + 1 < ratingArray.count; indexTwo++) {
+            for (NSInteger indexThree = 1; index + indexTwo + indexThree < ratingArray.count; indexThree++) {
+                if (([ratingArray[index] intValue] < [ratingArray[index + indexTwo] intValue] && [ratingArray[index + indexTwo] intValue] < [ratingArray[index + indexTwo + indexThree] intValue]) || ([ratingArray[index] intValue] > [ratingArray[index + indexTwo] intValue] && [ratingArray[index + indexTwo] intValue] > [ratingArray[index + indexTwo + indexThree] intValue])) {
+                    number++;
+                }
+            }
+        }
+    }
+    return number;
 }
 
 @end
--- Objc_Task3/ios.stage-task/Exercises/Exercise-4/LevelOrderTraversal.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-4/LevelOrderTraversal.m
@@ -1,5 +1,14 @@
 #import "LevelOrderTraversal.h"
 
 NSArray *LevelOrderTraversalForTree(NSArray *tree) {
+//    NSMutableArray *levelOrderedTree = [NSMutableArray new];
+//    NSInteger level = 0;
+//    for (id element in tree) {
+//        while (element != nil) {
+//            if (
+//            [levelOrderedTree addObject: [NSMutableArray new]];
+//            levelOrderedTree[level]
+//        }
+//    }
     return nil;
 }

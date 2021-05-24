276196a75faf591d69dedc69dca389f81019aab2
Julia Martsenko <jmartsenko@gmil.com>
2021-05-24 22:58:47 +0300

    problems in fourth ex

--- Objc_Task3/ios.stage-task/Exercises/Exercise-4/LevelOrderTraversal.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-4/LevelOrderTraversal.m
@@ -1,14 +1,38 @@
 #import "LevelOrderTraversal.h"
 
 NSArray *LevelOrderTraversalForTree(NSArray *tree) {
-//    NSMutableArray *levelOrderedTree = [NSMutableArray new];
-//    NSInteger level = 0;
-//    for (id element in tree) {
-//        while (element != nil) {
-//            if (
-//            [levelOrderedTree addObject: [NSMutableArray new]];
-//            levelOrderedTree[level]
-//        }
-//    }
-    return nil;
+    NSMutableArray *levelOrderedTree = [[NSMutableArray alloc] initWithArray:@[]];
+    if (tree.count > 0 && tree[0] == [NSNull null]) {
+        return @[];
+    }
+    NSMutableArray *levelSaver = [NSMutableArray new];
+    NSInteger index = 0;
+    NSInteger levelCounter = 0;
+    BOOL firstNill = true;
+    for (id element in tree) {
+        if (element == [NSNull null]) {
+            if (firstNill) {
+                [levelSaver removeObject:[NSNumber numberWithInteger:levelCounter]];
+                firstNill = false;
+            }
+            else {
+                levelCounter = [[levelSaver lastObject] integerValue];
+                firstNill = true;
+            }
+        }
+        else if (element != [NSNull null]){
+            if (levelCounter >= index) {
+                [levelSaver addObject:[NSNumber numberWithInteger:levelCounter]];
+                [levelOrderedTree addObject:[NSMutableArray new]];
+                index = levelCounter;
+            }
+            [levelOrderedTree[levelCounter] addObject:element];
+            levelCounter++;
+        }
+    }
+    if (levelOrderedTree.count > 1) {
+        [levelOrderedTree removeObject:[levelOrderedTree lastObject]];
+    }
+    NSArray *final = levelOrderedTree;
+    return final;
 }

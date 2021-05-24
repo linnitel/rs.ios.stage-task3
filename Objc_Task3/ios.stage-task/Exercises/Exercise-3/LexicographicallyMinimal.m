ce34663cf803e622f7233c1f61c638ebb3b2280d
Julia Martsenko <jmartsenko@gmil.com>
2021-05-24 06:26:19 +0300

    task three completed

--- Objc_Task3/ios.stage-task/Exercises/Exercise-3/LexicographicallyMinimal.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-3/LexicographicallyMinimal.m
@@ -9,7 +9,35 @@
 @implementation LexicographicallyMinimal
 
 -(NSString *)findLexicographicallyMinimalForString1:(NSString *)string1 andString2:(NSString *)string2 {
-    return nil;
+    NSString *str1 = [[NSString alloc] initWithString:string1];
+    NSString *str2 = [[NSString alloc] initWithString:string2];
+    NSMutableString *composedString = [NSMutableString new];
+    while (str1.length > 0 || str2.length > 0) {
+        if (str1.length == 0) {
+            unichar testCharTwo = [str2 characterAtIndex:0];
+            [composedString appendString:[NSString stringWithFormat: @"%C", testCharTwo]];
+            str2 = [str2 substringFromIndex:1];
+        }
+        else if (str2.length == 0) {
+            unichar testCharOne = [str1 characterAtIndex:0];
+            [composedString appendString:[NSString stringWithFormat: @"%C", testCharOne]];
+            str1 = [str1 substringFromIndex:1];
+        }
+        else {
+            unichar testCharOne = [str1 characterAtIndex:0];
+            unichar testCharTwo = [str2 characterAtIndex:0];
+            if (testCharOne <= testCharTwo) {
+                [composedString appendString:[NSString stringWithFormat: @"%C", testCharOne]];
+                str1 = [str1 substringFromIndex:1];
+            }
+            else {
+                [composedString appendString:[NSString stringWithFormat: @"%C", testCharTwo]];
+                str2 = [str2 substringFromIndex:1];
+            }
+        }
+    }
+    self->_resultString = composedString;
+    return self->_resultString;
 }
 
 @end
